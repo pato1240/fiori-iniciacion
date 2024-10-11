23702803af6eb799d4b145fa2e36fbd1d2586db7
pato1240 <valeryrojaso05@gmail.com>
2024-10-11 17:53:19 +0000

    First commit on development

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["invoices/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"invoices/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
